be8dc18a666e2af1418348f1fcb78103f5d40e67
Vyacheslav Bikbaev <la.sintez@gmail.com>
2023-09-10 13:28:08 +0400

    fix: autoplay breaking plugin

--- content.js
+++ content.js
@@ -4,6 +4,9 @@ const OBERSVER_TIMEOUT = 10_000;
 
 // For some reason, after navigation YouTube glances previos video title as current title. We track it to avoid double length in title.
 let prevTitle = "";
+// Fix for videos with length = 0:00, caused by Disable HTML5 Autoplay (Reloaded) https://chrome.google.com/webstore/detail/disable-html5-autoplay-re/cafckninonjkogajnihihlnnimmkndgf
+let prevLength = "";
+const EMPTY_LENGTH = "0:00";
 
 function isYouTubeVideoPage() {
   if (window.location.hostname === 'www.youtube.com' || window.location.hostname === 'm.youtube.com') {
@@ -13,7 +16,10 @@ function isYouTubeVideoPage() {
 }
 
 function updateTitle(videoLengthElement) {
-  const length = videoLengthElement?.textContent;
+  // Fix for videos with length = 0:00, caused by Disable HTML5 Autoplay (Reloaded) https://chrome.google.com/webstore/detail/disable-html5-autoplay-re/cafckninonjkogajnihihlnnimmkndgf
+  const lengthRaw = videoLengthElement?.textContent;
+  const length = lengthRaw === EMPTY_LENGTH ? prevLength : lengthRaw;
+  prevLength = length;
   if (isYouTubeVideoPage() && !document.title.includes(length) && document.title !== prevTitle) {
     const newTitle = `${length} ${document.title}`;
     document.title = newTitle;
@@ -66,6 +72,7 @@ waitForElement('title', function (titleElement) {
   const observer = new MutationObserver(function (mutationsList) {
     for (let mutation of mutationsList) {
       if (mutation.type === 'childList') {
+        console.log(document.querySelector("title"))
         waitForElement(VIDEO_LENGTH_SELECTOR, updateTitle);
       }
     }
